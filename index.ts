c3a54276e1a48b2deff814a6ef5636e109c89b62
DavidTorres09 <angeldtm09@gmail.com>
2023-04-05 13:06:59 -0400

    Proyect Completed

---
+++ index.ts
@@ -0,0 +1,90 @@
+import { log } from "console";
+import express, { Request, Response } from "express";
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const app = express();
+app.use(express.json());
+
+const prisma = new PrismaClient();
+
+app.post("/", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      username: username,
+      password: password,
+    },
+  });
+  res.json(user);
+});
+
+// app.post("/createManyUsers", async (req: Request, res: Response) => {
+//   const { userList } = req.body;
+//   const users = await prisma.user.createMany({
+//     data: userList,
+//   });
+//   res.json(users);
+// });
+
+// app.post("/createManyCars", async (req: Request, res: Response) => {
+//   const { carList } = req.body;
+//   const cars = await prisma.car.createMany({
+//     data: carList,
+//   });
+//   res.json(cars);
+// });
+
+app.get("/", async (req: Request, res: Response) => {
+  const cars = await prisma.user.findMany({
+    include: {
+      Cars: {
+        select: {
+          id: true,
+          model: true,
+          year: true,
+        }
+      }
+    }
+  })
+  
+  
+  res.json(cars);
+});
+
+
+app.get("/byId/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const user = await prisma.user.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+  res.json(user);
+});
+
+app.put("/", async (req: Request, res: Response) => {
+  const { id, username } = req.body;
+  const updateUser = await prisma.user.update({
+    where: {
+      id: id,
+    },
+    data: {
+      username: username,
+    },
+  });
+  res.json(updateUser);
+});
+
+app.delete("/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const deletedUser = await prisma.user.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+  res.json(deletedUser);
+});
+
+app.listen(3001, () => {
+  console.log("SERVER RUNNING ON PORT 3001");
+});
